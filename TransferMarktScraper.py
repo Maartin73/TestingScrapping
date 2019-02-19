80ef251354436d22fa00edae6321891dc2695445
Maartin73 <34165561+Maartin73@users.noreply.github.com>
2019-02-19 17:25:33 +0100

    Add files via upload

---
+++ TransferMarktScraper.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+headers = {'User-Agent': 
+           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
+page = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=alle&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&w_s="
+pageTree = requests.get(page, headers=headers)
+pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+
+
+Links = pageSoup.find_all("a", href=True)
+
+MorePages = []
+for a in Links: 
+    if a['href'].find('/transfers/transferrekorde/statistik/top/saison_id/alle/land_id//ausrichtung//spielerposition_id//altersklasse//leihe//w_s//plus/0/galerie/0/page/')!=-1:
+        MorePages.append(a['href'])
+
+
+
+Players = pageSoup.find_all("a", {"class": "spielprofil_tooltip"})
+Values = pageSoup.find_all("td", {"class": "rechts hauptlink"})
+
+                
+PlayersList = []
+ValuesList = []
+
+for i in range(0,25):
+                PlayersList.append(Players[i].text)
+                ValuesList.append(Values[i].text)
+
+for x in range (1,10):
+        page = "https://www.transfermarkt.co.uk"+MorePages[x]
+        pageTree = requests.get(page, headers=headers)
+        pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+
+        Players = pageSoup.find_all("a", {"class": "spielprofil_tooltip"})
+        Values = pageSoup.find_all("td", {"class": "rechts hauptlink"})
+        for i in range(0,25):
+                PlayersList.append(Players[i].text)
+                ValuesList.append(Values[i].text)
+
+df = pd.DataFrame({"Players":PlayersList,"Values":ValuesList})
+print(df)
+#df.to_csv('example.csv', sep='\t', index=False, header=False, encoding='utf-8')
